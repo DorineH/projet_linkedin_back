d5079db5ba74fbdf5bf6dee541261ffacd13bb6f
DorineH <dorinematildehenry@gmail.com>
2026-04-08 18:20:22 +0200

    main fast fix network redenr bug

--- app/db/session.py
+++ app/db/session.py
@@ -1,23 +1,28 @@
 import os
 from typing import AsyncGenerator
+from fastapi.datastructures import URL
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from .models import Base
 from app.core.config import settings
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.asyncio import create_async_engine
+import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL environment variable is not set")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# if not DATABASE_URL:
+#     raise RuntimeError("DATABASE_URL environment variable is not set")
 
-engine = create_async_engine(
-    DATABASE_URL,
-    echo=False,
-    future=True,
-    pool_pre_ping=True,
-    connect_args={"user": "postgres.cawpdbwfbsizrtbnttgk"}
-    # connect_args={
-    #     "statement_cache_size": 0,
-    # }
+url = URL.create(
+    drivername="postgresql+psycopg",
+    username=os.environ.get("DB_USER", "postgres.cawpdbwfbsizrtbnttgk"),
+    password=os.environ.get("DB_PASSWORD"),
+    host=os.environ.get("DB_HOST", "aws-0-eu-west-3.pooler.supabase.com"),
+    port=int(os.environ.get("DB_PORT", 6543)),
+    database=os.environ.get("DB_NAME", "postgres"),
+    query={"sslmode": "require"}
 )
+
+engine = create_async_engine(url)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
