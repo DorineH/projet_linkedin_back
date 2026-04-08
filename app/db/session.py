b0a120e3f5a4010bd37d88bb27b6b9d061183654
DorineH <dorinematildehenry@gmail.com>
2026-04-08 17:45:46 +0200

    main fast fix

--- app/db/session.py
+++ app/db/session.py
@@ -12,10 +12,10 @@ engine = create_async_engine(
     DATABASE_URL,
     echo=False,
     future=True,
-    pool_pre_ping=True,
-    connect_args={
-        "statement_cache_size": 0,
-    }
+    pool_pre_ping=True
+    # connect_args={
+    #     "statement_cache_size": 0,
+    # }
 )
 
 AsyncSessionLocal = async_sessionmaker(
